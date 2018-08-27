4d31b7fb4720a16a349ce6ef307fb3b819a86cfc
Dhananjay <d.gupta@3mdigital.co.in>
2018-08-27 11:30:19 +0530

    api key fixed testing

--- missedcallphoneverification/src/main/java/com/mobtexting/missedcallphoneverification/service/MobtextingServices.java
+++ missedcallphoneverification/src/main/java/com/mobtexting/missedcallphoneverification/service/MobtextingServices.java
@@ -154,7 +154,8 @@ public class MobtextingServices extends IntentService{
                 });
             }else{
                 errCode = MobtextingResultReceiver.RESULT_CODE_ERROR;
-                bundle.putSerializable(MobtextingResultReceiver.PARAM_EXCEPTION, new ServerResponse("Dear developer. Don't forget to configure <meta-data android:name=\"mobtexting.api_key\" android:value=\"testValue\"/> in your AndroidManifest.xml file.", false, 500));
+                bundle.putSerializable(MobtextingResultReceiver.PARAM_EXCEPTION, new ServerResponse("Dear developer. Don't forget to configure <meta-data android:name=\"mobtexting.api_key\" android:value=\"testValue\"/> in your AndroidManifest.xml file or api " +
+                        "key can not be empty.", false, 500));
                 if (resultReceiver != null) {
                     resultReceiver.send(errCode, bundle);
                 }
